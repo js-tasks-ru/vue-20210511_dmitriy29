5985b86965ed79b7c7c24ec8621b12d9ca271209
dvol79 <kidvol2002@gmail.com>
2021-05-17 14:32:01 +0300

    Add solution for Counter Button

--- 01-basics/01-CounterButtonDraft/script.js
+++ 01-basics/01-CounterButtonDraft/script.js
@@ -1,3 +1,17 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
-// Требуется создать Vue приложение
+const app = new Vue({
+  el: "#app",
+
+  data() {
+    return {
+      counter: 0
+    }
+  },
+
+  methods: {
+    increase() {
+      this.counter++;
+    }
+  },
+});
