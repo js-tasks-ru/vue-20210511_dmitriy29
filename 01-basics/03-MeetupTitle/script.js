aee61fdc556f89a29d85ec805c742e339fdf267b
dvol79 <kidvol2002@gmail.com>
2021-05-18 09:39:01 +0300

    Add solution for 01.03 Meetup Title

--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -1,3 +1,35 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
-// Требуется создать Vue приложение
+const app = new Vue({
+  data() {
+    return {
+      meetups: [1,2,3,4,5],
+      selected: null,
+      title: '',
+    }
+  },
+
+  watch: {
+    selected: {
+      // deep: true,
+      immediate: true,
+      handler(newValue) {
+        if (newValue > 0) {
+          this.getMeetup(newValue);
+        }
+      },
+    }
+  },
+
+  methods: {
+    getMeetup(id) {
+      fetch(`https://course-vue.javascript.ru/api/meetups/${id}`)
+      .then(res => res.json())
+      .then(data => this.title = data.title);
+    }
+  },
+
+  template: `#app`,
+});
+
+app.$mount('#app');
