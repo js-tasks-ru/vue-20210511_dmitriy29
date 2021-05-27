06a6313126d0428156f3c4a8254abf4460c8a2e7
dvol79 <kidvol2002@gmail.com>
2021-05-27 20:58:06 +0300

    Add solution for 03.01 Counter Button Vue

--- 03-sfc/01-CounterButton-vue/components/CounterButton.vue
+++ 03-sfc/01-CounterButton-vue/components/CounterButton.vue
@@ -1,7 +1,29 @@
-<template><div></div></template>
+<template>
+  <div>
+    <button type="button" @click="increase">{{ count }}</button>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  methods: {
+    increase() {
+      this.$emit('increment', this.count + 1);
+    },
+  },
+};
 </script>
 
 <style></style>
