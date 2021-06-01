1e63cbded919a672220986ce9984cae917809029
dvol79 <kidvol2002@gmail.com>
2021-06-01 10:25:57 +0300

    Add solution for 04.02 Not Found

--- 04-spa/02-NotFound/App.vue
+++ 04-spa/02-NotFound/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <header>
+    <header v-if="!$route.params.pathMatch">
       <nav>
         <ul>
-          <li><router-link to="/page-a">/page-a</router-link></li>
-          <li><router-link to="/page-b">/page-b</router-link></li>
+          <li><router-link to="/page-a" exact>/page-a</router-link></li>
+          <li><router-link to="/page-b" exact>/page-b</router-link></li>
           <li><router-link to="/page-unknown">/page-unknown</router-link></li>
         </ul>
       </nav>
