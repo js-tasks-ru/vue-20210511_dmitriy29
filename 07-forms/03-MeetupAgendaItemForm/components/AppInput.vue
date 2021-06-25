296dc537e5a8c5dc6434fa48d8c2f0fe3a0f8b49
dvol79 <kidvol2002@gmail.com>
2021-06-25 09:45:22 +0300

    Add solution for 07.03 MeetupAIForm

---
+++ 07-forms/03-MeetupAgendaItemForm/components/AppInput.vue
@@ -0,0 +1,173 @@
+<template>
+  <div
+    class="input-group"
+    :class="{
+      'input-group_icon': hasIcons,
+      'input-group_icon-left': leftIcon,
+      'input-group_icon-right': rightIcon,
+    }"
+  >
+    <slot v-if="$slots['left-icon']" name="left-icon" />
+
+    <component
+      :is="inputComponent"
+      ref="input"
+      :value.prop="value"
+      class="form-control"
+      :class="{ 'form-control_rounded': rounded, 'form-control_sm': small }"
+      v-bind="$attrs"
+      v-on="listeners"
+    ></component>
+
+    <slot v-if="$slots['right-icon']" name="right-icon" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppInput',
+
+  inheritAttrs: false,
+
+  model: {
+    prop: 'value',
+    event: 'input',
+  },
+
+  props: {
+    small: {
+      type: Boolean,
+      default: false,
+    },
+
+    rounded: {
+      type: Boolean,
+      default: false,
+    },
+
+    multiline: {
+      type: Boolean,
+      default: false,
+    },
+
+    value: {
+      type: String,
+    },
+  },
+
+  data() {
+    return {
+      hasIcons: false,
+      leftIcon: false,
+      rightIcon: false,
+    };
+  },
+
+  computed: {
+    inputComponent() {
+      return this.multiline ? 'textarea' : 'input';
+    },
+
+    listeners() {
+      return {
+        ...this.$listeners,
+        input: ($event) => {
+          this.$emit('input', $event.target.value);
+        },
+
+        change: ($event) => {
+          this.$emit('change', $event.target.value);
+        },
+      };
+    },
+  },
+
+  mounted() {
+    this.updateHasIcons();
+  },
+
+  updated() {
+    this.updateHasIcons();
+  },
+
+  methods: {
+    updateHasIcons() {
+      this.leftIcon = !!this.$slots['left-icon'];
+      this.rightIcon = !!this.$slots['right-icon'];
+      this.hasIcons = this.leftIcon || this.rightIcon;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-control {
+  padding: 12px 16px;
+  height: 52px;
+  border-radius: 8px;
+  border: 2px solid var(--blue-light);
+  font-family: 'Nunito', sans-serif;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 28px;
+  color: var(--body-color);
+  transition: 0.2s all;
+  background-color: var(--white);
+  outline: none;
+  box-shadow: none;
+}
+
+.form-control::placeholder {
+  font-weight: 400;
+  color: var(--blue-2);
+}
+
+.form-control:focus {
+  border-color: var(--blue);
+}
+
+textarea.form-control {
+  width: 100%;
+  min-height: 211px;
+}
+
+.form-control.form-control_rounded {
+  border-radius: 26px;
+}
+
+.form-control.form-control_sm.form-control_rounded {
+  border-radius: 22px;
+}
+
+.form-control.form-control_sm {
+  padding: 8px 16px;
+  height: 44px;
+  border-radius: 4px;
+}
+
+.input-group {
+  position: relative;
+}
+
+.input-group .form-control {
+  width: 100%;
+}
+
+.input-group.input-group_icon .form-control {
+  padding-left: 50px;
+}
+
+.input-group.input-group_icon .icon {
+  position: absolute;
+  top: 50%;
+  transform: translate(0, -50%);
+}
+
+.input-group.input-group_icon.input-group_icon-left .icon:first-child {
+  left: 16px;
+}
+
+.input-group.input-group_icon.input-group_icon-right .icon:last-child {
+  right: 16px;
+}
+</style>
